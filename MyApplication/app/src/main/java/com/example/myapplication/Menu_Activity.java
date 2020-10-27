a305562d6e04837e5cdff5efb9b1e975c8c23c3f
Guechan <65199893+Guechan@users.noreply.github.com>
2020-10-27 17:38:12 -0300

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Menu_Activity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Activity {
+
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.menu);
+
+        }
+    }
+}
